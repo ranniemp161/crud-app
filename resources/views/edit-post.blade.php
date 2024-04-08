9e04ecb5a8b98695949b0f7cfa21bec946851e88
rannieperalta <rannieperalta06@gmail.com>
2024-04-08 20:09:51 +0800

    adding tailwind to the project

--- resources/views/edit-post.blade.php
+++ resources/views/edit-post.blade.php
@@ -4,16 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ 
+
     <title>Edit Post</title>
 </head>
 <body>
     <h1>Edit Post</h1>
+    <div style="flex;">
     <form action="/edit-post/{{$post->id}}" method="POST">
         @csrf
         @method('PUT')
         <input type="text" name='title' value="{{$post->title}}">
         <textarea name="body">{{$post->body}}</textarea>
-        <button>Save Changes</button>
+        <button style=
+        "background-color: blue; 
+        color:white; 
+        border-radius:10px;
+        "
+        >
+        Save Changes</button>
     </form>
+    </div>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Homme</title>
+    @vite('resources/css/app.css')
+    <title>Home</title>
 </head>
-<body>
+<body >
     @auth
         <p>Congrats you're log in</p>
         <form action="/logout" method="POST">
         @csrf
-            <button>Log out</button>
+            <button class="bg-blue-950">Log out</button>
         </form>
         <div style="border: 3px solid black;">
             <h2>Create a New Post</h2>
